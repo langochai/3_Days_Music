f206965e0d458682052a67f5c774453ac93c21b6
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-07-24 09:37:51 +0700

    update navbar

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -5,22 +5,13 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Link, useNavigate} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {logout} from "../../redux/features/auth/authSlice.jsx";
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
+import AccountMenu from "../AccountMenu.jsx";
 
 export default function NavBar() {
     const auth = useSelector(state => state.auth);
-    const userLoginJSON = localStorage.getItem('userLogin');
-    const userLogin = JSON.parse(userLoginJSON);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userLogin');
-        dispatch(logout());
-        navigate('/login');
-    }
+
 
     return (
         <Box sx={{flexGrow: 1}}>
@@ -38,15 +29,13 @@ export default function NavBar() {
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                         Menu
                     </Typography>
-                    {userLogin && <p>{userLogin.lastName}</p>}
                     {!auth.isAuth ? (
                         <Link to='/login'>
                             <Button color="inherit">Login</Button>
                         </Link>
                     ) : (
-                        <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                        <AccountMenu/>
                     )}
-
                 </Toolbar>
             </AppBar>
         </Box>
